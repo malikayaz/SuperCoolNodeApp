200e6e9b2531df4fcff1427481b7a9aa67b4e42a
malikayazkhan <muhammad.ayaz@tekenable.com>
2018-12-07 14:59:43 +0500

    make modifications in web.config

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var router           = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port=process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
     console.log('URL: ' + req.url);
